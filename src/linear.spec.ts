c3c82f1d6d40fe181fbd56041aa899a91e659784
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-04-12 21:36:33 +0200

    Add Lin — the linear type

---
+++ src/linear.spec.ts
@@ -0,0 +1,10 @@
+import { Lin } from "./linear";
+
+describe('Lin', () => {
+  it('Lin should not be readable twice', () => {
+    const un = 12;
+    const lin = new Lin(un);
+    expect(lin.read()).toEqual(un);
+    expect(() => lin.read()).toThrowError();
+  });
+});
---
+++ src/linear.ts
@@ -0,0 +1,16 @@
+export class Lin<T> {
+  #value: T | undefined;
+
+  constructor(value: T) {
+    this.#value = value;
+  }
+
+  read(): T {
+    if (this.#value === undefined) {
+      throw Error('Cannot read the linear value twice.')
+    }
+    const value: T = this.#value;
+    this.#value = undefined;
+    return value;
+  }
+}
