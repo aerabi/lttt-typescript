6aa24cd9d02578e13489bd625f1cf51c76e4bf78
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-04-13 13:40:35 +0200

    Add flatBind and fix Prettier to avoid parens

--- src/single.spec.ts
+++ src/single.spec.ts
@@ -1,18 +1,26 @@
 import { Single } from './single';
 
 describe('Single', () => {
-  it('bind', (done) => {
+  it('bind', done => {
     const single = Single.fromValue(12);
     single
-      .bind((x) => x * 2)
-      .bind((x) => expect(x).toEqual(24))
+      .bind(x => x * 2)
+      .bind(x => expect(x).toEqual(24))
       .bind(done);
   });
 
   it('bind twice', () => {
     expect.assertions(2);
     const single = Single.fromValue(12);
-    single.bind((x) => expect(x).toEqual(12));
-    single.bind(_ => {}).catch(error => expect(error).toBeTruthy());
+    single.bind(x => expect(x).toEqual(12));
+    single.bind(fail).catch(error => expect(error).toBeTruthy());
+  });
+
+  it('flatBind', done => {
+    const single = Single.fromValue(12);
+    single
+      .flatBind(x => Single.fromValue(x * 2))
+      .bind(x => expect(x).toEqual(24))
+      .bind(done);
   });
 });
